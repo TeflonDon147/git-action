7f0fde290847f3e19cbc4e04619d25b3439c94fe
Teflon1 <tonycares4uall@yahoo.com>
2025-02-10 11:19:56 -0500

    release0

--- .github/workflows/release.config.js
+++ .github/workflows/release.config.js
@@ -1,12 +1,14 @@
-export const branches = "main";
-export const plugins = [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/git',
-    '@semantic-release/github',
-    ['@semantic-release/npm',
+module.exports = {
+    branches: "main",
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/git',
+      '@semantic-release/github',
+      ['@semantic-release/npm',
         {
-            'npmPublish': false
+          'npmPublish': false
         }
+      ]
     ]
-];
+}
